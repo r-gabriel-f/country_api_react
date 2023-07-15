08c1d9258afbba56a3874c0ed28617018158a291
robert <r.gabriel.f@outlook.com>
2023-07-15 09:39:03 -0400

    mostrando datos de paises

--- src/conponents/VerApi.js
+++ src/conponents/VerApi.js
@@ -1,29 +1,45 @@
 import { useState, useEffect } from "react";
 import React from "react";
-
+import "../conponents/VerApi.css"
 const VerApi = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
-      .then((Response) => Response.json())
+      .then((response) => response.json())
       .then((data) => setData(data));
   }, []);
-
   return (
     <section>
-      <h1>Aplicacando Api</h1>
+      <h1>Aplicando Api</h1>
       <div className="paises">
-        <ul>
-            {data?.map((user)=>(
-                <li>{user.capital}</li>
-                
+        <table>
+          <thead>
+            <tr>
+              <th>Bandera</th>
+              <th>Nombre</th>
+              <th>Capital</th>
+              <th>Población</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((auxpais) => (
+              <tr key={auxpais.name.common}>
+                <td>
+                  <img src={auxpais.flags.svg} alt={auxpais.name.common} />
+                </td>
+                <td>{auxpais.name.common}</td>
+                <td>{auxpais.capital}</td>
+                <td>{auxpais.population}</td>
+              </tr>
             ))}
-           
-        </ul>
+          </tbody>
+        </table>
       </div>
     </section>
   );
 };
+
 export default VerApi;
+
 
