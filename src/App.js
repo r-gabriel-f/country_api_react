213f2a124f6c034c3c449032a9dfab6e8d44b8c4
robert <r.gabriel.f@outlook.com>
2023-07-26 15:26:13 -0400

    v1 f2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import VerPaises from './conponents/VerPaises';
-import { VerRegion } from './conponents/VerRegion';
-import { NavBar } from './conponents/NavBar';
-import CountryInfo from './conponents/CountryInfo';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import VerPaises from "./conponents/VerPaises";
+import { VerRegion } from "./conponents/VerRegion";
+import { NavBar } from "./conponents/NavBar";
+import CountryInfo from "./conponents/CountryInfo";
 import { React, useState, useEffect } from "react";
 
 function App() {
@@ -11,26 +16,27 @@ function App() {
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
-      .then((data) => setData(data)); console.log(data);
-    
+      .then((data) => setData(data));
+    console.log(data);
   }, []);
-  
+
   return (
     <Router>
-      <div>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<VerPaises />} />
-          <Route path="/region" element={<VerRegion />} />
-          <Route path="/pais/:countryName" element={<CountryInfo data={data} />} />
-          {/*<Route path="/" element={<Navigate to="/paises" />} />*/}
+      <NavBar />
+      <VerPaises />
+      <Routes>
+        <Route path="/" element={<VerPaises />} />
+        <Route path="/region" element={<VerRegion />} />
+        <Route
+          path="/pais/:countryName"
+          element={<CountryInfo data={data} />}
+        />
+        {/*<Route path="/" element={<Navigate to="/paises" />} />*/}
 
-          {/* Resto de las rutas */}
-        </Routes>
-      </div>
+        {/* Resto de las rutas */}
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
